84c064e4487d2927fa1f76c95352270c3986d910
abdhulla-k <abdhullak149@gmail.com>
2023-09-20 21:23:23 +0530

    learned how to create statefull widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // import 'package:first_app/styled_text.dart';
+import 'package:first_app/dice_roller.dart';
 
 // using variables
 var startAlignment = Alignment.topLeft;
@@ -12,10 +13,6 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> gradientColors;
 
-  void rollDice() {
-    //execute
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -26,29 +23,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
+      child: const Center(
         // child: StyledText('Welcome to First App!'),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-with-five-dots_318-60136.png',
-              width: 200.0,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                  ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  )),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
